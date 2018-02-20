1fc7ed5fb0ad76c9618516bacb0af96771be0078
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-02-20 18:04:40 -0500

    Initial commit

---
+++ FallingObjectsLeoK/FallingObjectsLeoKForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace FallingObjectsLeoK
+{
+    partial class frmFallingObjects
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRequest = new System.Windows.Forms.Label();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblConclusion = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblTooSmall = new System.Windows.Forms.Label();
+            this.lblGround = new System.Windows.Forms.Label();
+            this.lblLetter = new System.Windows.Forms.Label();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblRequest
+            // 
+            this.lblRequest.AutoSize = true;
+            this.lblRequest.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequest.Location = new System.Drawing.Point(12, 38);
+            this.lblRequest.Name = "lblRequest";
+            this.lblRequest.Size = new System.Drawing.Size(561, 22);
+            this.lblRequest.TabIndex = 0;
+            this.lblRequest.Text = "Please enter the amount of seconds since you\'ve dropped the object:";
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(727, 24);
+            this.mnuMenu.TabIndex = 1;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(92, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblConclusion
+            // 
+            this.lblConclusion.AutoSize = true;
+            this.lblConclusion.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConclusion.Location = new System.Drawing.Point(12, 203);
+            this.lblConclusion.Name = "lblConclusion";
+            this.lblConclusion.Size = new System.Drawing.Size(305, 22);
+            this.lblConclusion.TabIndex = 2;
+            this.lblConclusion.Text = "The height of the object is currently:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(589, 38);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 3;
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(188, 110);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(112, 35);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(354, 203);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 22);
+            this.lblAnswer.TabIndex = 5;
+            // 
+            // lblTooSmall
+            // 
+            this.lblTooSmall.AutoSize = true;
+            this.lblTooSmall.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTooSmall.Location = new System.Drawing.Point(376, 116);
+            this.lblTooSmall.Name = "lblTooSmall";
+            this.lblTooSmall.Size = new System.Drawing.Size(298, 22);
+            this.lblTooSmall.TabIndex = 6;
+            this.lblTooSmall.Text = "Please enter a number 0 or greater.";
+            // 
+            // lblGround
+            // 
+            this.lblGround.AutoSize = true;
+            this.lblGround.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGround.Location = new System.Drawing.Point(394, 203);
+            this.lblGround.Name = "lblGround";
+            this.lblGround.Size = new System.Drawing.Size(133, 22);
+            this.lblGround.TabIndex = 7;
+            this.lblGround.Text = "On the ground!";
+            // 
+            // lblLetter
+            // 
+            this.lblLetter.AutoSize = true;
+            this.lblLetter.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLetter.Location = new System.Drawing.Point(376, 148);
+            this.lblLetter.Name = "lblLetter";
+            this.lblLetter.Size = new System.Drawing.Size(229, 22);
+            this.lblLetter.TabIndex = 8;
+            this.lblLetter.Text = "Please enter numbers only!";
+            // 
+            // frmFallingObjects
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 261);
+            this.Controls.Add(this.lblLetter);
+            this.Controls.Add(this.lblGround);
+            this.Controls.Add(this.lblTooSmall);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblConclusion);
+            this.Controls.Add(this.lblRequest);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmFallingObjects";
+            this.Text = "Falling Objects by Leo Kay";
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRequest;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblConclusion;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblTooSmall;
+        private System.Windows.Forms.Label lblGround;
+        private System.Windows.Forms.Label lblLetter;
+    }
+}
+
---
+++ FallingObjectsLeoK/FallingObjectsLeoKForm.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Leo Kay
+ * February 20, 2018
+ * Course Code: ICS3U
+ * This program calculates the height of an object that has been dropped 100 meters and the user inputs the number of seconds since it has fallen.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObjectsLeoK
+{
+    public partial class frmFallingObjects : Form
+    {
+        public frmFallingObjects()
+        {
+            InitializeComponent();
+
+            //hide some labels
+            lblTooSmall.Hide();
+            lblGround.Hide();
+            lblLetter.Hide();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //close the program
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            //allow these words to store numbers
+            double seconds, answer;
+
+            //Store the users input in the word seconds
+            seconds = double.Parse(txtInput.Text);
+
+            //calculate the answer
+            answer = (100 - 0.5 * 9.81 * Math.Pow(seconds, 2));
+
+            //Round the answer
+            answer = Math.Round(answer, 2);
+
+            //convert the answer to a string
+            this.lblAnswer.Text = Convert.ToString(answer) + " meters";
+
+            /*If the number is less than 0, tell them not to enter negative numbers
+             * If the number is greater than 0, hide the too small label and show the answer
+             * If the number is equal to 0, show the answer and hide the too small label.
+             */
+            if (seconds < 0)
+            {
+                lblTooSmall.Show();
+                lblAnswer.Hide();
+                lblGround.Hide();
+                lblLetter.Hide();
+            }
+            else
+            {
+                if (seconds > 0)
+                {
+                    lblAnswer.Show();
+                    lblTooSmall.Hide();
+                    lblGround.Hide();
+                    lblLetter.Hide();
+                }
+
+                if (seconds == 0)
+                {
+                    lblAnswer.Show();
+                    lblTooSmall.Hide();
+                    lblLetter.Hide();
+                }
+
+                //If the answer is 0 or less, say the object has hit the ground
+                if (answer < 0)
+                {
+                    lblGround.Show();
+                    lblAnswer.Hide();
+                    lblTooSmall.Hide();
+                    lblLetter.Hide();
+                }
+            }
+        }
+
+        //Make it so that the user can only enter numbers
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                    (e.KeyChar != '-') && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+                lblLetter.Show();
+                lblGround.Hide();
+                lblAnswer.Hide();
+                lblTooSmall.Hide();
+            }
+        }
+    }
+}
+
+
